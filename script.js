d5427a6557463fcb974c08bd27c56a3ebf772a07
Horlan <horlankelwyn@gmail.com>
2021-10-20 17:08:33 -0300

    atualização-principal

--- script.js
+++ script.js
@@ -1,40 +1,68 @@
-let dados = {
+// definindo o objeto 
+let movies = {
     imagem: '',
     titulo: '',
     genero: '',
     nota: 0,
+    id:0
 };
-
+// ouvindo o evento submit c addEventListener e com o preventDefault "descartando o f5 q o submit dá na pg qd envia"
+// definindo uma var form para receber os valores do form do HTML c getElementById
+let form = document.getElementById('form')
+form.addEventListener('submit',(evento)=>{
+    evento.preventDefault()
+});
+// pegando os valores dos inputs para add um novo filme na lista movie
 const atualizarDados = () =>{
-console.log('test');
-dados ={
-    imagem: document.getElementById('imagem').value,
-    titulo: document.getElementById('titulo').value,
-    genero: document.getElementById('genero').value,
-    nota: document.getElementById('nota').value,
-};
-
-document.getElementById('debug').innerHTML = JSON.stringify(dados)
+// Reatribuindo os valore vindo dos inputs ao meu obj movies
+    movies ={
+        imagem: document.getElementById('imagem').value,
+        titulo: document.getElementById('titulo').value,
+        genero: document.getElementById('genero').value,
+        nota: document.getElementById('nota').value,
+        id:new Date().getTime()
+    };
 }
-console.log(dados);
+// inserindo os valores dos inputs dentro da var movie, atribuindo class e data-id para a 'div' a ser criada
+// que vai formar o elemento com o card do movie, e inserindo ele no html c template literals
 const montaItem = () => {
-const item = document.createElement('div')
-const id = new Date.getTime();
-item.insertAdjacentHTML('beforeEnd',`
-<i class="fa-solid fa-trash excluir fa-flash" style="--fa-flash-opacity: 0.1; --fa-flash-scale: 1.25;" ></i>
-<img src="${dados.imagem}" class="mobile" alt=""></img>
-<h3>${dados.titulo}</h3>
-<h4>${dados.genero}</h4>
-<h1 class="notas">${dados.nota}</h1>               
-`)
-return item
+    const movie = document.createElement('div')
+    movie.setAttribute('data-id', movies.id)
+    movie.setAttribute('class','cards')
+    movie.insertAdjacentHTML('beforeEnd',
+    `   
+        <img src="${movies.imagem}" class="mobile" alt="imagem do filme"></img>
+        <div class="div-infos">
+            <h3>${movies.titulo}</h3>
+            <h4>${movies.genero}</h4>
+            <h1>${movies.nota}</h1>   
+        </div>
+        <button class="delete-movie" data-id="${movies.id}">
+            <i class="fa-solid fa-trash excluir fa-flash" style="--fa-flash-opacity: 0.1; --fa-flash-scale: 1.25;" ></i>
+        </button> 
+
+    `)
+    
+return movie
 
 }
-
+// função q adc um movie individual dentro do elemento addMovies
 const cadastrar = () =>{
     document.getElementById('addMovies').appendChild(montaItem())
-
+    movies = {}
+    imagem.value = ''
+    titulo.value = ''
+    genero.value = ''
+    nota.value = ''
+    id.value = ''
 }
 
-// // definindo uma var form para receber os valores do form do HTML c getElementById
+
+
+// addMovies.push(movies)
+// console.log(addMovies);
+
+
+
+
 
--- test.js
+++ test.js
@@ -1,50 +1,18 @@
-// let form = document.getElementById('form')
-// // console.log(form);============
-// // ouvindo o evento submit c addEventListener e com o preventDefault "descartando o f5 q o submit dá na pg qd envia"
-// let movies = [];
-// form.addEventListener('submit', (evento) =>{
-//     evento.preventDefault();
-// // pegando os valores dos inputs para add um novo filme na lista movie
-//     const inputTitulo = document.getElementById('titulo').value;
-//     const inputImagem = document.getElementById('imagem').value;
-//     const inputGenero = document.getElementById('genero').value;
-//     const inputNota = document.getElementById('nota').value;
-// // definindo meu obj movie para receber os valore vindo dos inputs
-//     const movie = {
-//         titulo: inputTitulo,
-//         imagem: inputImagem,
-//         genero: inputGenero,
-//         nota: inputNota,
-//     }
-// // inserindo os valores dos inputs dentro da var movies c o push
-//     movies.push(movie)
-//     console.log(movies)
-//     const addMovies = document.getElementById('addMovies')
-//     console.log(addMovies);
-//     movies.map((movie) => {
-//         addMovies.innerHTML('beforeEnd',
-//         `
-//         <div class="meus-filmes-favoritos">
-//         <button>
-//         <i class="fa-solid fa-trash excluir fa-flash" style="--fa-flash-opacity: 0.1; --fa-flash-scale: 1.25;" ></i>
-//         </button>
-//             <img src=${movie.imagem} class="mobile"alt=""></img>
-//             <h3>${movie.titulo}</h3>
-//             <h4>${movie.genero}</h4>
-//             <h1 class="notas">${movie.nota}</h1>
 
-//         </div>  
-        
-//         `)
+<div class="cards imgs">
+    <i class="fa-solid fa-trash excluir fa-flash" style="--fa-flash-opacity: 0.1; --fa-flash-scale: 1.25;" ></i>
+    <img src="madagascar.webp" class="mobile" alt="capa Madagascar 3">
+    <h3>Madagascar 3</h3>
+    <h4>Infantil/Comédia</h4>
+    <h1 class="notas">10</h1>
+</div>
 
-        // console.log(addMovies);===========
-//         addMovies.append(movie)
-//     })
-// })
-
-/* <i class="fa-solid fa-trash excluir fa-flash" style="--fa-flash-opacity: 0.1; --fa-flash-scale: 1.25;" ></i> */
-
-// função q adc um movie individual dentro do array de movie
-
-
+{/* 
+    nota input:out-of-range{
+    border:6px solid;
+    border-color: red;
+    color: red;
+    font-size: 1.5rem;
+    font-weight: bolder;
+    background-color: aliceblue; */}
 
