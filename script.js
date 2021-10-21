763e1b601923f6a58a44e72c0ddedd4583185aeb
Horlan <horlankelwyn@gmail.com>
2021-10-21 09:06:37 -0300

    atualização

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+let MovieList = []
 // definindo o objeto 
 let movies = {
     imagem: '',
@@ -20,34 +21,37 @@ const atualizarDados = () =>{
         titulo: document.getElementById('titulo').value,
         genero: document.getElementById('genero').value,
         nota: document.getElementById('nota').value,
-        id:new Date().getTime()
+        id: new Date().getTime()
     };
+    
+
 }
 // inserindo os valores dos inputs dentro da var movie, atribuindo class e data-id para a 'div' a ser criada
 // que vai formar o elemento com o card do movie, e inserindo ele no html c template literals
 const montaItem = () => {
-    const addmovie = document.createElement('div')
-    addmovie.setAttribute('data-id', movies.id)
-    addmovie.setAttribute('class','cards')
-    const movieExist = document.querySelector(`[data-id='${addmovie.id}']`)
+    addToStorage()
+    const ItemMovie = document.createElement('div')
+    ItemMovie.setAttribute('data-id', movies.id)
+    ItemMovie.setAttribute('class','cards')
+    // const movieExist = document.querySelector(`[data-id='${ItemMovie.id}']`)
     
-    addmovie.insertAdjacentHTML('beforeEnd',
+    ItemMovie.insertAdjacentHTML('beforeEnd',
     `   
-        <img src="${movies.imagem}" class="mobile" alt="imagem do filme"></img>
+        <img src="${movies.imagem}" alt="imagem do filme"></img>
         <div class="div-infos">
             <h3>${movies.titulo}</h3>
             <h4>${movies.genero}</h4>
             <h1>${movies.nota}</h1>   
         </div>
         <button class="delete-movie" data-id="${movies.id}" onClick="Delete(${movies.id})">
-            <i class="fa-solid fa-trash excluir fa-flash" style="--fa-flash-opacity: 0.1;" ></i>
+            <i class="fa-solid fa-trash excluir fa-flip" style="--fa-animation-duration: 3s;"></i>
         </button> 
 
     `)
 
-    return addmovie
+    return ItemMovie
 }
-
+MovieList.push(movies)
 // função q adc um movie individual dentro do elemento addMovies
 const cadastrar = () =>{
     document.getElementById('addMovies').appendChild(montaItem())
@@ -58,13 +62,30 @@ const cadastrar = () =>{
     nota.value = '',
     id.value = ''
 }
-
+console.log('minha lista',MovieList)
 const Delete = (id) =>{
     const deleted = document.querySelector(`[data-id='${id}']`)
     deleted.remove();
+    // const index = MovieList.findIndex(movies => movies.id === id)
+    // MovieList.splice(index,1)
+}
+
+const addToStorage = () => {
+    localStorage.setItem('ListMovies',JSON.stringify(movies))
 }
 
 
+const renderListMovieStorage = () => {
+
+    const movieStorage = localStorage.getItem('ListMovies')
+    if(movieStorage){
+        MovieList = JSON.parse(movieStorage)
+        MovieList.map((movies,index) =>{
+            montaItem(movies)
+        })
+    }
+}
+renderListMovieStorage()
 // metodo antigo de delete que eu tava testando 
 // const Delete = (id) =>{
 //     const index = movies.findIndex(movies => movies.id === id)
